701deaff7a7334ae6d92bc57af99b39b33d5e671
ADANKON <wiltordadankon720@gmail.com>
2025-08-02 16:34:07 +0200

    Demarage avec l'app User:creation et migration du models user

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'livraisons',
     'transactions',
     'commentaires',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,49 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 # Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("L'adresse e-mail est obligatoire")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self,email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Le superuser doit avoir is_staff=true")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Le superuser doit avoir is_superuser")
+        
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser,PermissionsMixin):
+    ROLE_CHOICES =(
+        ('client', 'Client'),
+        ('livreur', 'livreur'),
+        ('admin', 'Admin'),
+        ('staff', 'Personnel'),
+    )    
+
+    email = models.EmailField(unique=True)
+    nom = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=20,blank=True)
+    role = models.CharField(max_length=10,choices=ROLE_CHOICES,default="client")
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nom']
+
+    def __str__(self):
+        return self.email
